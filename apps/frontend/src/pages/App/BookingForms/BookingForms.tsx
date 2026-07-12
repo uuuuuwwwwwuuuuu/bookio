e4f0e382eb7e0a8a254eecf01f1e7f0425a5dc29
Denis <devorlich@gmail.com>
2026-07-12 10:55:09 +0300

    Implemented booking form errors handling and loading

--- apps/frontend/src/pages/App/BookingForms/BookingForms.tsx
+++ apps/frontend/src/pages/App/BookingForms/BookingForms.tsx
@@ -1,11 +1,32 @@
-import { type FC } from 'react';
+import { useCallback, type FC } from 'react';
 import styles from './BookingForms.module.scss';
 import { useGetBookingForms } from '@api/bookingForms/getBookingForms';
-import { useGetOrganization } from '@api/organizations/getOrganizationData';
+import { useParams } from 'react-router-dom';
+import { Button } from '@bookio/ui';
+import { Spinner } from '@components/Spinner/Spinner';
 
 export const BookingForms: FC = () => {
-    const {} = useGetOrganization();
-    const { data: bookingForms, isPending } = useGetBookingForms();
+    const { id } = useParams();
+    const { data: bookingForms, isPending, error, refetch } = useGetBookingForms(id);
+
+    const handleRetry = useCallback(() => {
+        refetch();
+    }, [refetch]);
+
+    if (isPending) {
+        return <div className={styles.loadingContainer}>
+            <Spinner size={6} />
+        </div>
+    }
+
+    if (error) {
+        return (
+            <div className={styles.errorContainer}>
+                <h2>{error.message}</h2>
+                <Button variant="primary-outlined" onClick={handleRetry}>Retry</Button>
+            </div>
+        );
+    }
 
     return <div className={styles.bookingForms}></div>;
 };
